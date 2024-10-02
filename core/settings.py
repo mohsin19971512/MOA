4f9dfa403511cc43180c57c96ddc0825e74461d0
unknown <mohsin19971512@gmail.com>
2024-10-02 11:55:19 +0300

    update ssl

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-tdn^p-3ecek17@nal9n!=vuvvvs&huca1175)!%gdkkl#4a(_0
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['moa-fh5j.onrender.com', '127.0.0.1']
 
 
 INSTALLED_APPS = [
